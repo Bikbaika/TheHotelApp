e5421fb7a08cafd5395101a59862f6d016d0f194
Valeria Evseeva <Kaleria4444@gmail.com>
2019-05-29 21:53:37 +0300

    Work with services

--- TheHotelApp/Data/ApplicationDbContext.cs
+++ TheHotelApp/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using TheHotelApp.Models;
 
 namespace TheHotelApp.Data
 {
@@ -11,6 +12,21 @@ namespace TheHotelApp.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<ApplicationUser> Users { get; set; }
+        public DbSet<RoomType> RoomTypes { get; set; }
+        public DbSet<ServiceList> ServiceLists { get; set; }
+        public DbSet<UsedService> UsedServices { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
         }
     }
 }
---
+++ TheHotelApp/Services/GenericHotelService.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using TheHotelApp.Data;
+
+namespace TheHotelApp.Services
+{
+    public class GenericHotelService<TEntity> : IGenericHotelService<TEntity> where TEntity : class
+    {
+        private readonly ApplicationDbContext _context;
+        protected DbSet<TEntity> DbSet;
+
+        public GenericHotelService(ApplicationDbContext context)
+        {
+            _context = context;
+            DbSet = context.Set<TEntity>();
+        }
+        public async Task CreateItemAsync(TEntity entity)
+        {
+            DbSet.Add(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteItemAsync(TEntity entity)
+        {
+            DbSet.Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task EditItemAsync(TEntity entity)
+        {
+            _context.Update(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllItemsAsync()
+        {
+            return await DbSet.ToArrayAsync();
+        }
+
+        public async Task<TEntity> GetItemByIdAsync(Guid? id)
+        {
+            if (id == null)
+            {
+                return null;
+            }
+
+            return await DbSet.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<TEntity>> SearchForAsync(Expression<Func<TEntity, bool>> expression)
+        {
+            return await DbSet.Where(expression).ToArrayAsync();
+        }
+    }
+}
